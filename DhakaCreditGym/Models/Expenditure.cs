6380322ef1f48eb78c4f6d9932d5bb4fe9deac5d
sonyjgomes <sonygcccul@gmail.com>
2018-10-17 17:07:26 +0600

    create Expediture Model class,BL,Insert,Update,search,select Method

---
+++ DhakaCreditGym/Models/Expenditure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace DhakaCreditGym.Models
+{
+    public class Expenditure
+    {
+        [Key]
+        public int exp_id { get; set; }
+        public int emp_id { get; set; }
+        public string exp_purpose { get; set; }
+        public string exp_for { get; set; }
+        public string exp_description { get; set; }
+        public Decimal exp_amount { get; set; }
+        public DateTime created_date { get; set; }
+        public string created_by { get; set; }
+    }
+}
